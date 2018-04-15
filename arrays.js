8dcd0d1adf0980ff1bd6128db396419c74c2c0e4
Jennifer Snyder <jlsnyder09@gmail.com>
2018-04-15 14:45:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,5 +27,9 @@ function accessElementInArray(array,index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array
-  
+}
+
+function removeElementFromBeginningOfArray(array)  {
+  array.slice(1);
+  return array
 }
